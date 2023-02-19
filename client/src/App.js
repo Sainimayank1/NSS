0dcbdd1f94ec3e314644904219458b60c2aa9313
Mayank <mayanksaini4455@gmail.com>
2023-02-19 19:44:59 +0530

    Add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-
+import Main_navbar from "./Components/Navbars/Main_navbar";
+import Navbar from "./Components/Navbars/Navbar";
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <>
+      <Main_navbar/>
+      <Navbar/>
+    </>
   );
 }
 
---
+++ client/src/Components/Navbars/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className='Navbar blue-bg'>
+      <div className='Navbar-left'>
+      
+        <a href='./'>Home</a>
+      </div>
+      <div className='Navbar-right'>
+          <ul>
+            <li><a href='./'>Posts</a></li>
+            <li><a href='./'>Create Post</a></li>
+            <li><a href='./'>Logout</a></li>
+          </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
