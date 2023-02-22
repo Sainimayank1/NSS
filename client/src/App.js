e7933585c8c53616864e8ca1026dd1fff0f9d2e0
Manu Kumar Pal <manukumarpal34@gmail.com>
2023-02-22 17:16:33 +0530

    adding carousel

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import Main_navbar from "./Components/Navbars/Main_navbar";
 import Navbar from "./Components/Navbars/Navbar";
+import MyCarousel from "./Components/Carousel/MyCarousel";
+import React from "react";
+
 function App() {
   return (
     <>
       <Main_navbar/>
       <Navbar/>
+      <MyCarousel />
     </>
   );
 }
