fc8aed80b49f97f4b6abfec63f422582b71432a4
Manu Kumar Pal <manukumarpal34@gmail.com>
2023-02-25 20:52:12 +0530

    login and register add

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import Main_navbar from "./Components/Navbars/Main_navbar";
 import Navbar from "./Components/Navbars/Navbar";
 import MyCarousel from "./Components/Carousel/MyCarousel";
+import Main1 from "./Components/Main/Main1";
 import React from "react";
+import Organization from "./Components/Organization/Organization";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Main_navbar/>
       <Navbar/>
       <MyCarousel />
+      <Main1 />
+      <Organization />
     </>
   );
 }
---
+++ server/models/Token.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const tokenSchema = new Schema({
+	userId: {
+		type: Schema.Types.ObjectId,
+		required: true,
+		unique: true,
+		ref:"user"
+	},
+	token: { type: String, required: true },
+},
+{timestamps:true});
+
+export default mongoose.model("token", tokenSchema);
