0b1ebb1a3b7e70efd9c0ba0c4249d87d5654a831
Manu Kumar Pal <manukumarpal34@gmail.com>
2023-03-09 09:43:52 +0530

    add gallery section

--- client/src/Components/Gallery/Gallery.jsx
+++ client/src/Components/Gallery/Gallery.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Gallery.css';
-import { Carousel } from 'react-bootstrap';
+import SimpleImageSlider from "react-simple-image-slider";
+
 
 const Gallery = () => {
   return (
@@ -8,80 +9,17 @@ const Gallery = () => {
     <div className='heading'>
         <h3>Gallery Section</h3>
     </div>
-    <div className='view-gallery'>
-    <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src='./images/nss1.jpg' alt="First slide"/>
-          <Carousel.Caption>
-            {/* <h3>First slide label</h3> */}
-            {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src='./images/nss2.jpg' alt="Second slide" />
-  
-          <Carousel.Caption>
-            {/* <h3>Second slide label</h3> */}
-            {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src='./images/nss3.png' alt="Third slide" />
-  
-          <Carousel.Caption>
-            {/* <h3>Third slide label</h3> */}
-            {/* <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p> */}
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src='./images/nss1.jpg' alt="First slide"/>
-          <Carousel.Caption>
-            {/* <h3>First slide label</h3> */}
-            {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src='./images/nss1.jpg' alt="First slide"/>
-          <Carousel.Caption>
-            {/* <h3>First slide label</h3> */}
-            {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src='./images/nss1.jpg' alt="First slide"/>
-          <Carousel.Caption>
-            {/* <h3>First slide label</h3> */}
-            {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src='./images/nss1.jpg' alt="First slide"/>
-          <Carousel.Caption>
-            {/* <h3>First slide label</h3> */}
-            {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+     <div className='main1-container'>
+    <div className='first-div'>
+    <img src='./picture/g1.jpg' alt='photo' />
     </div>
+     <div className='second-div'>
+     <img src='./picture/g2.jpg' alt='photo'></img>
+    </div>
+    </div>
+    
    </>
   )
 }
-
 
 export default Gallery
