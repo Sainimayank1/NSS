1260e68fb34ba8150d31589e24fdd3bdea6c19eb
Manu Kumar Pal <manukumarpal34@gmail.com>
2023-03-22 22:56:32 +0530

    add some images and modify card section

--- client/src/Components/Gallery/Gallery.jsx
+++ client/src/Components/Gallery/Gallery.jsx
@@ -36,6 +36,10 @@ const Gallery = () => {
     <a href=''><img src='./picture/p5.jpg' alt='img' className='img-gallery' ></img></a>
     <a href=''><img src='./picture/p7.jpg' alt='img' className='img-gallery' ></img></a>
     <a href=''><img src='./picture/p3.jpg' alt='img' className='img-gallery' ></img></a>
+    <a href=''><img src='./picture/bd1.jpg' alt='img' className='img-gallery' ></img></a>
+    <a href=''><img src='./picture/bd2.jpg' alt='img' className='img-gallery' ></img></a>
+    <a href=''><img src='./picture/bd4.jpg' alt='img' className='img-gallery' ></img></a>
+    <a href=''><img src='./picture/p8.jpg' alt='img' className='img-gallery' ></img></a>
   </Carousel>
    </div>
 
