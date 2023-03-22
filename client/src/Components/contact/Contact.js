b55dc7d64d69b21aa84892d2edd09221453bae0e
Mayank <mayanksaini4455@gmail.com>
2023-03-23 00:13:13 +0530

    add contact us page

--- client/src/Components/contact/Contact.js
+++ client/src/Components/contact/Contact.js
@@ -3,74 +3,66 @@ import './contact.css'
 import mayank from './mayank.jpeg'
 import manu from './manu.jpeg'
 import mridul from './mridul.jpeg'
+import fluuter from './flutter.png'
+import mern from './mern.png'
+import linkedin from "./linked.webp"
 
 function Contact() {
-  return (
-    <div class="pro-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="card">
-                            <div class="img2"><img src={mayank} alt=""/></div> 
-                                <div class="main">
-                                    <h2>Manu Kumar Pal</h2>
-                                    <h3>Mern stack Developer</h3>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus dignissimos consequuntur fugit consectetur aliquam. Rem, neque perspiciatis. Esse odio voluptatum placeat! Magni, deserunt!</p>
-                                </div>
-                                <div class="social-icon">
-                                    <i class="fa-brands fa-square-facebook"></i>
-                                    <i class="fa-brands fa-square-instagram"></i>
-                                    <i class="fa-brands fa-square-twitter"></i>
-                                    <i class="fa-brands fa-linkedin"></i>
-                                </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card">
-                            <div class="img2"><img src={manu} alt=""/></div>
-                                <div class="main">
-                                        <h2>Mayank Saini </h2>
-                                        <h3>Mern Stack Developer</h3>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus dignissimos consequuntur fugit consectetur aliquam. Rem, neque perspiciatis. Esse odio voluptatum placeat! Magni, deserunt!</p>
-                                </div>
-                                <div class="social-icon">
-                                        <i class="fa-brands fa-square-facebook"></i>
-                                        <i class="fa-brands fa-square-instagram"></i>
-                                        <i class="fa-brands fa-square-twitter"></i>
-                                        <i class="fa-brands fa-linkedin"></i>
-                                    </div>
-                    </div>
-                </div>
+    return (
+        <div class="pro-area">
+            <div class="container-contact">
+                <div class="row contact-row">
                     <div class="col-md-4">
-                        <div class="card">
-                                <div class="img2"><img src={mridul} alt=""/></div>
-                                    <div class="main">
-                                         <h2>Person Three</h2>
-                                         <h3>Unknown Developer</h3>
-                                         {/* <div class="wrapper">
-                                            <div class="static-txt">I'm a</div>
-                                            <ul class="dynamic-txts">
-                                              <li><span>YouTuber</span></li>
-                                              <li><span>Designer</span></li>
-                                              <li><span>Developer</span></li>
-                                              <li><span>Freelancer</span></li>
-                                            </ul>
-                                          </div>  */}
-                                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus dignissimos consequuntur fugit consectetur aliquam. Rem, neque perspiciatis. Esse odio voluptatum placeat! Magni, deserunt!</p>
-                                         </div>
-                                         <div class="social-icon">
-                                            <i class="fa-brands fa-square-facebook"></i>
-                                            <i class="fa-brands fa-square-instagram"></i>
-                                            <i class="fa-brands fa-square-twitter"></i>
-                                            <i class="fa-brands fa-linkedin"></i>
-                                 </div>
-                           </div>
-                       </div>
+                        <div class="card-contact">
+                            <div class="img2"><img src={mayank} alt="" /></div>
+                            <div class="main-contact">
+                                <h2>Mayank Saini </h2>
+                                <h4>(Mern stack Developer)</h4>
+                                <h6>Builded: Web Version of NSS</h6>
+                                <img src={mern}></img>
+                            </div>
+                            <div className='contact-follow'>
+                                <div>Follow Us On :</div>
+                                <img src={linkedin}></img>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card-contact">
+                            <div class="img2"><img src={manu} alt="" /></div>
+                            <div class="main-contact">
+                                <h2>Manu Kumar Pal</h2>
+                                <h4>(Mern stack Developer)</h4>
+                                <h6>Builded: Web Version of NSS</h6>
+                                <img src={mern}></img>
+                            </div>
+                            <div className='contact-follow'>
+                                <div>Follow Us On :</div>
+                                <img src={linkedin}></img>
+                            </div>
+                        </div>
 
                     </div>
+                    <div class="col-md-4 align-center">
+                        <div class="card-contact">
+                            <div class="img2"><img src={mridul} alt="" /></div>
+                            <div class="main-contact">
+                                <h2>Mridul Vij</h2>
+                                <h4>(Flutter Developer)</h4>
+                                <h6>Builded: App Version of NSS</h6>
+                                <img src={fluuter}></img>
+                            </div>
+                            <div className='contact-follow'>
+                                <div>Follow Us On :</div>
+                                <img src={linkedin}></img>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
-  )
+        </div>
+    )
 }
 
 export default Contact
